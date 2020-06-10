84f9304f310fa1739fabddc653d2c18fc06b5a9d
Milkitic <Milkitic@126.com>
2020-06-10 15:00:22 +0800

    move files

--- YobotExtension/Properties/AssemblyInfo.cs
+++ YobotExtension/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("PcrYobotExtension")]
+[assembly: AssemblyTitle("YobotExtension")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PcrYobotExtension")]
+[assembly: AssemblyProduct("YobotExtension")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
