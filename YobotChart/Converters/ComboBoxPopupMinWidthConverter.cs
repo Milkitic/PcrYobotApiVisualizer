46ad1423a2b2c94fd990d26872da2426b264c91c
Milkitic <Milkitic@126.com>
2020-06-16 16:23:02 +0800

    combobox style

---
+++ YobotChart/Converters/ComboBoxPopupMinWidthConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace YobotChart.Converters
+{
+    public class ComboBoxPopupMinWidthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var val = (double)value;
+            if (val == 0)
+            {
+                return 265;
+            }
+
+            return val + 65;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ YobotChart/Converters/ComboBoxPopupVerticalOffsetConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace YobotChart.Converters
+{
+    public class ComboBoxPopupVerticalOffsetConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int offset = 0;
+            if (value is int index)
+            {
+                if (index >= 1)
+                {
+                    offset = -index * 34;
+                }
+            }
+
+            int standard;
+            if (parameter is string s)
+            {
+                standard = int.Parse(s);
+            }
+            else if (parameter is null)
+            {
+                standard = 0;
+            }
+            else
+            {
+                standard = (int)parameter;
+            }
+
+            return standard + offset;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
